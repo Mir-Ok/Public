2e0d5a8a019c5ef0e70e9a4b43df172fa131c46a
Oksana <73831834+Mir-Ok@users.noreply.github.com>
2023-06-19 13:16:03 +0300

    Resolve temp.jsx

--- temp.jsx
+++ temp.jsx
@@ -1,51 +1,62 @@
 import { useState } from 'react';
 
+const user = {
+	firstName: 'Jane',
+	lastName: 'Jacobs'
+}
 
-function App() {
-	
-	const user = {
-		firstName: 'Jane',
-		lastName: 'Jacobs'
-	 }
+function CreateInput({userData, setUserData, isEdit, label, value }) { 	
 
-	const [isEdit, setIsEdit] = useState(true)
-	const [userData, setUserData] = useState(user)
-	
-	function CreateInput({label, value}) { 
-	  
-	  return (
-		 isEdit ? 
-			  <label>
-				  <span>{label}</span>
-				  <input 
-						type="text" 
-						defaultValue={userData[value]} 
-						onChange={ 
-							(e) => {setUserData({...userData, [value]: e.target.value})
-							console.log(userData)
-							console.log(e.target.value)}
-						}           
-				 />
-			  </label>
+	return (
+		isEdit ? 
+			<label>
+				<span>{label}</span>
+				<input 
+					type="text" 
+					defaultValue={userData[value]} 
+					onChange={ (e) => {setUserData({...userData, [value]: e.target.value}) }}           
+				/>
+			</label>
 			:
 			<p>{label} {userData[value]}</p>
-		 )
-	}
- 
-	function CreateButton() {
-	  return (
-		 <button type="submit" onClick={ ()=> setIsEdit(!isEdit)}>
-			{isEdit ? 'Save Profile' : 'Edit Profile'}
-		 </button>
-	  )
-	}
+	)
+}
+
+function CreateButton({ isEdit, setIsEdit }) {
+
 	return (
-	  <form onSubmit={ (e) => e.preventDefault() } >
-		 <CreateInput label='First name: ' value='firstName'/>
-		 <CreateInput label='Last name: ' value='lastName'/>
-		 <CreateButton />
-		 <p><i>Hello, {userData.firstName} {userData.lastName}!</i></p>
-	  </form>
+		<button type="submit" onClick={ ()=> setIsEdit(!isEdit)}>
+			{isEdit ? 'Save Profile' : 'Edit Profile'}
+		</button>
+	)
+}
+
+function App() {	
+
+	const [userData, setUserData] = useState(user)	
+	const [isEdit, setIsEdit] = useState(true)
+
+	return (
+	   <form onSubmit={ (e) => e.preventDefault() } >
+			<CreateInput 
+				userData={userData} 
+				setUserData={setUserData} 
+				isEdit={isEdit} 
+				label='First name: ' 
+				value='firstName'
+			/>
+			<CreateInput 
+				userData={userData} 
+				setUserData={setUserData} 
+				isEdit={isEdit} 
+				label='Last name: ' 
+				value='lastName'/>
+			<CreateButton 
+				isEdit={isEdit} 
+				setIsEdit={setIsEdit} 
+			/>
+			<p><i>Hello, {userData.firstName} {userData.lastName}!</i></p>
+	   </form>
 	);
 }
 
